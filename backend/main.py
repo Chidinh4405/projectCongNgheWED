54a3798a2c7979ff9139d611e9710b02d408a7af
Chidinh4405 <164855965+Chidinh4405@users.noreply.github.com>
2026-05-31 05:57:11 +0700

    fix CORS

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ from routers import upload, analysis, chart
 app = FastAPI(title="Data Dashboard API")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://project-cong-nghe-wed.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
